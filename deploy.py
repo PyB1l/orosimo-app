1e12cff9da917a8e60cc286f34f8b7be8388d6c0
PyB1l <vpapavasil@gmail.com>
2016-11-04 19:06:03 +0200

    ready for action

--- deploy.py
+++ deploy.py
@@ -2,15 +2,12 @@ import app
 import bottle
 import settings
 
-app_container = bottle.Bottle()
-
-app_container.mount('/beta-version', app.wsgi)
 
 if __name__ == '__main__':
 
     bottle.debug(True)
 
     bottle.run(
-        app=app_container,
+        app=app.wsgi,
         **settings.SERVER_OPTS
     )
